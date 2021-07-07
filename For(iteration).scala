7446feeae0e0899c8ec1bd0d7c0ec08d1bfe1483
arunarunganesh14 <36407941+arunarunganesh14@users.noreply.github.com>
2021-07-07 12:48:27 +0530

    Create For(iteration).scala

---
+++ For(iteration).scala
@@ -0,0 +1,9 @@
+object MainObject {  
+   def main(args: Array[String]) {  
+        var list = List(1,2,3,4,5,6,7,8,9)          // Creating a list  
+        for( i <- list){                         // Iterating the list  
+            println(i)  
+        }  
+          
+   }  
+}
