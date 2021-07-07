df747f6f134ad104b4c6bf99f5c1bd8469fd2017
arunarunganesh14 <36407941+arunarunganesh14@users.noreply.github.com>
2021-07-07 12:52:46 +0530

    Create Function(=).scala

---
+++ Function(=).scala
@@ -0,0 +1,10 @@
+object MainObject {  
+   def main(args: Array[String]) {  
+        var result = functionExample()          // Calling function  
+        println(result)  
+    }  
+    def functionExample() = {       // Defining a function  
+          var a = 10  
+          a  
+    }  
+} 
