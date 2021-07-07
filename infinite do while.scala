4f6857982b6639eb4ec811a9a2aeede2e8f0e93e
arunarunganesh14 <36407941+arunarunganesh14@users.noreply.github.com>
2021-07-07 12:47:16 +0530

    Create infinite do while.scala

---
+++ infinite do while.scala
@@ -0,0 +1,10 @@
+object MainObject {  
+   def main(args: Array[String]) {  
+        var a = 10;                        
+        do {  
+            println( a );  
+            a = a + 2;                     
+        }  
+        while( true)                        
+   }  
+}  
