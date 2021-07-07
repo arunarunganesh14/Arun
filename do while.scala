7a86d989909e7bb00a2f54691bd2e798d7b0eb3e
arunarunganesh14 <36407941+arunarunganesh14@users.noreply.github.com>
2021-07-07 12:46:44 +0530

    Create do while.scala

---
+++ do while.scala
@@ -0,0 +1,10 @@
+object MainObject {  
+   def main(args: Array[String]) {  
+        var a = 10;         // Initialization  
+        do {  
+            println( a );  
+            a = a + 2;      // Increment  
+        }  
+        while( a <= 20 )     // Condition  
+   }  
+}  
