8c4e5146cef8ff2f1e1a9efd9f3957fb4528080f
arunarunganesh14 <36407941+arunarunganesh14@users.noreply.github.com>
2021-07-07 12:44:14 +0530

    Create nested if.scala

---
+++ nested if.scala
@@ -0,0 +1,20 @@
+var number:Int = 85  
+if(number>=0 && number<50){  
+    println ("fail")  
+}  
+else if(number>=50 && number<60){  
+    println("D Grade")  
+}  
+else if(number>=60 && number<70){  
+    println("C Grade")  
+}  
+else if(number>=70 && number<80){  
+    println("B Grade")  
+}  
+else if(number>=80 && number<90){  
+    println("A Grade")  
+}  
+else if(number>=90 && number<=100){  
+    println("A+ Grade")  
+}  
+else println ("Invalid")  
