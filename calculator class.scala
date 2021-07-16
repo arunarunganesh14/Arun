bb0997ede3f085f7ee153e148ee896b5b251ef1d
arunarunganesh14 <36407941+arunarunganesh14@users.noreply.github.com>
2021-07-16 18:13:43 +0530

    Update calculator class.scala

--- calculator class.scala
+++ calculator class.scala
@@ -8,26 +8,10 @@ class Calculator {
     require(b !=0, "denominator can not be 0")
     a/b
   }
-  def %(a:Int,b:Int):Int = a%b
-
 }
-object CalculatorDemo
-{
-  def main(args: Array[String]): Unit =
-  {
-    val calc = new Calculator()
+   val calc = new Calculator()
 
-    var scanner = new Scanner(System.in)
-    val a = scanner.nextInt
-    val b= scanner.nextInt
-    println(a)
-    println(b)
-
-    println("Addition: " + calc.+(a, b))
-    println("Subtraction: " + calc.-(a, b))
-    println("Multiplication: " + calc.*(a, b))
-    println("Division: " + calc./(a, b))
-    println("Modulo Division: " + calc.%(a, b))
-
-  }
-}
+  println("Addition: " + calc.+(15, 3))
+  println("Subtraction: " + calc.-(15, 3))
+  println("Multiplication: " + calc.*(15, 3))
+  println("Division: " + calc./(15, 3))
