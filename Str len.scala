cc20558b07a7ad4e086fd04a38ca7dfb32ac3374
arunarunganesh14 <36407941+arunarunganesh14@users.noreply.github.com>
2021-07-19 12:39:17 +0530

    Create Str len.scala

---
+++ Str len.scala
@@ -0,0 +1,13 @@
+object Strlen
+{
+    var str1 = "Hello!"
+    var str2: String = "Scala HandsOn"
+    
+    def main(args: Array[String]) 
+    {
+        var LEN1 = str1.length();
+        var LEN2 = str2.length();
+        println("String 1:" + str1 + ", Length :" + LEN1);
+        println("String 2:" + str2 + ", Length :" + LEN2);
+    }
+}
