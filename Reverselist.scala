32c16fb0efe9e5d641d5e68655ebd3edf91f0e8f
arunarunganesh14 <36407941+arunarunganesh14@users.noreply.github.com>
2021-07-23 19:07:15 +0530

    Create Reverselist.scala

---
+++ Reverselist.scala
@@ -0,0 +1,9 @@
+object reverse
+{
+    def main(args:Array[String])
+    {
+        val mylist = List(1, 2, 3, 4, 5) 
+        println("Original list:" + mylist)
+        println("Reverse list:" + mylist.reverse)
+    }
+}
